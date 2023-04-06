fddd89a25845a50b014b248faaa3dfce09b4ce3c
Aansh Kotian <76598799+Aone52@users.noreply.github.com>
2023-04-06 14:27:51 -0400

    create cities

---
+++ a4-main/a4main/island/src/main/java/IslandGenerator/generator/CityGenerator.java
@@ -0,0 +1,11 @@
+package IslandGenerator.generator;
+
+import java.util.List;
+
+import IslandGenerator.model.Tiles.Tile;
+
+public class CityGenerator {
+    public void generate_cities(List<Tile> tiles, int num_cities){
+        
+    }
+}
--- a4-main/a4main/island/src/main/java/IslandGenerator/model/Island.java
+++ a4-main/a4main/island/src/main/java/IslandGenerator/model/Island.java
@@ -6,11 +6,15 @@ import IslandGenerator.generator.IslandGenerator;
 
 import IslandGenerator.model.Tiles.Tile;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
+import ca.mcmaster.cas.se2aa4.a2.pathfinder.model.Edge.Edge;
+import ca.mcmaster.cas.se2aa4.a2.pathfinder.model.Node.Node;
 
 public class Island {
     private List<Tile> tiles;
     private List<River> rivers;
     private Mesh islandMesh;
+    private List<Node> cities;
+    private List<Edge> roads;
 
 
 
@@ -33,4 +37,22 @@ public class Island {
     public List<Tile> getTiles(){
         return this.tiles;
     }
+
+    public void setCities(List<Node> cities){
+        this.cities = cities;
+    }
+
+    public List<Node> getCities(){
+        return this.cities;
+    }
+
+    public void setRoads(List<Edge> roads){
+        this.roads = roads;
+    }
+
+    public List<Edge> getRoads(){
+        return this.roads;
+    }
+
+
 }
